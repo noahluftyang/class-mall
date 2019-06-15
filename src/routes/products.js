537ba422c3a5b519c4f1903b3ec70e020a399bc1
Noah Yang <ykd1217@gmail.com>
2019-06-15 10:27:17 +0900

    Display product information

--- src/routes/products.js
+++ src/routes/products.js
@@ -2,8 +2,12 @@ import React, { memo, useEffect, useState } from 'react';
 
 import * as mallApi from '../api';
 
+function formatPrice(price) {
+  return Intl.NumberFormat('ko', { currency: 'KRW', style: 'currency' }).format(price);
+}
+
 function sortByScore(items) {
-  return items;
+  return items.sort((formerItem, latterItem) => latterItem.score - formerItem.score);
 }
 
 export const Products = memo(() => {
@@ -25,8 +29,12 @@ export const Products = memo(() => {
 
   return (
     <div>
-      {productItems.map(productItem => (
-        <div>{JSON.stringify(productItem)}</div>
+      {productItems.map(({ coverImage, id, price, title }) => (
+        <div key={`product-item-${id}`}>
+          <img alt={title} src={coverImage} />
+          <h4>{title}</h4>
+          <p>가격: {formatPrice(price)}</p>
+        </div>
       ))}
     </div>
   );
